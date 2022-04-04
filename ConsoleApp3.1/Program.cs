b9772783cd6b74fe74e4234cc3df01320169af1b
D1SS007 <101556566+D1SS007@users.noreply.github.com>
2022-04-04 19:31:11 +0300

    Update Program.cs

--- ConsoleApp3.1/Program.cs
+++ ConsoleApp3.1/Program.cs
@@ -1,16 +1,15 @@
-﻿using System;
+using System;
 
 namespace ConsoleApp3._1
 {
     internal class Program
     {
         static void Main(string[] args)
-        { 
+        {
             Random random = new Random();
-            
             int sumOfSecondRow = 0;
             int multiplyOfFirstColumn = 1;
-            int amountOfColumns = random.Next(4,10);
+            int amountOfColumns = random.Next(4, 10);
             int amountOfRows = random.Next(2, 5);
             int startNumbers = 1;
             int endNumbers = 99;
@@ -19,25 +18,24 @@ namespace ConsoleApp3._1
 
             for (int i = 0; i < array.GetLength(0); i++)
             {                
-                for (int j = 0; j < array .GetLength(1); j++)
+                for (int j = 0; j < array.GetLength(1); j++)
                 {
                     array[i, j] = random.Next(startNumbers, endNumbers);
 
-                    Console.Write(array[i, j] + " ");
-
-                    if(i== 1)
-                    {
-                        sumOfSecondRow += array[i,j];
-                    }
-                    if(j== 0)
-                    {
-                        multiplyOfFirstColumn*=array[i,j];
-                    }
+                    Console.Write(array[i, j] + " ");                    
                 }
-                Console.WriteLine();                
+                Console.WriteLine();
+            }
+             for (int j = 0; j < amountOfColumns; j++)
+            {
+                sumOfSecondRow+=array[1,j];
+            }
+             for(int i = 0; i < amountOfRows; i++)
+            {
+                multiplyOfFirstColumn*=array[i,0];
             }
             Console.WriteLine($"Сумма второго ряда = {sumOfSecondRow}");
-            Console.WriteLine($"Произведение первой колонны = {multiplyOfFirstColumn}");
+            Console.WriteLine("Произведение первой колонны = " + multiplyOfFirstColumn);
         }
     }
 }
