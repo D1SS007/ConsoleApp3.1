98bc1e18aa2cebbf7c42b2e5b973558f6e367ad1
D1SS007 <101556566+D1SS007@users.noreply.github.com>
2022-04-04 19:38:01 +0300

    Update Program.cs

--- ConsoleApp3.1/Program.cs
+++ ConsoleApp3.1/Program.cs
@@ -17,24 +17,30 @@ namespace ConsoleApp3._1
             int[,] array = new int[amountOfRows, amountOfColumns];
 
             for (int i = 0; i < array.GetLength(0); i++)
-            {                
+            {    
+                
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
                     array[i, j] = random.Next(startNumbers, endNumbers);
 
                     Console.Write(array[i, j] + " ");                    
                 }
+                
                 Console.WriteLine();
             }
+            
              for (int j = 0; j < amountOfColumns; j++)
             {
                 sumOfSecondRow+=array[1,j];
             }
+            
              for(int i = 0; i < amountOfRows; i++)
             {
                 multiplyOfFirstColumn*=array[i,0];
             }
+            
             Console.WriteLine($"Сумма второго ряда = {sumOfSecondRow}");
+            
             Console.WriteLine("Произведение первой колонны = " + multiplyOfFirstColumn);
         }
     }
